a7849ab5a8dc4f382d12afe7331276597d35ef8f
blazingXtremez <riteishy2j@gmail.com>
2017-07-27 19:47:51 -0400

    data bindings, ngif and so on

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServers = false;
+  serverCreationStatus = 'No Server was created';
+  serverName = '';
+  serverCreated = false;
+  // userName = '';
+  // users = false;
 
   constructor() {
     setTimeout(() => {
       this.allowNewServers = true;
     }, 2000);
-   }
+  }
 
   ngOnInit() {
   }
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Server Name ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log(event.target.value);
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(this.serverName);
+  }
+
+  // onResetUser() {
+  //   this.userName = '';
+  // }
+
+  // enableReset() {
+  //   this.users = true;
+  // }
 
 }
